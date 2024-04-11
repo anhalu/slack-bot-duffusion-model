ed337feb6f13c1db37d3a19462db2f706a3f3d70
DngBack <bachduong7103@gmail.com>
2024-04-11 16:17:33 +0700

    Push

--- bot.py
+++ bot.py
@@ -1,61 +1,51 @@
-from slack_bolt import App
+import os
+from slack_bolt import App 
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from decouple import config
+from pathlib import Path 
+from dotenv import load_dotenv
+from slack_sdk import WebClient
 
-app = App(token=config("SLACK_BOT_TOKEN"))
+# Loads environment variables from .env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
+# Initializes slack app with bot tokens
+app = App(token=os.environ['SLACK_TOKEN'])
+client = WebClient(token=os.environ['SLACK_TOKEN'])
 
-def generate_image(prompt):
-    pass
-    # return image
-
-
+#Listen and handle slash command for stable diffusion image generation
 @app.command("/create-image")
 def create_image(ack, command, client):
+
+    #Acknowledge command request from slack
     ack()
 
+    #Get prompt from command text and add midjourney style to it
     prompt = f"mdjrny-v4 style {command['text']}"
 
+    #Post message to channel indicating that image is being generated
     initial_message = client.chat_postMessage(channel=command["channel_id"], text="Generating image...")
 
-    with open('test.jpg', 'rb') as f:
-        image = f.read()
+    with open('Image.jpg', 'rb') as f:
+            image = f.read()
 
-    '''
-        server -> fastapi -> public ip -> 
-    '''
+    re = client.files_upload_v2(file=image, filename='Image.jpg', channels=command["channel_id"], filetype='jpg')
 
-    # image = generate_image(prompt)
-
-    re = client.files_upload_v2(file=image, filename='test.jpg', channels=command["channel_id"], filetype='jpg')
-    image_url = re['file']['url_private']
-
-    # client.chat_update(
-    #     channel=command["channel_id"],
-    #     ts=initial_message["ts"],
-    #     blocks= [
-    #         {
-    #             "type": "section",
-    #             "text": {
-    #                 "type": "mrkdwn",
-    #                 "text": "Image generated! :white_check_mark:"
-    #         }
-    #         },
-    #         {
-    #             "type": "image",
-    #             "title": {
-    #                 "type": "plain_text",
-    #                 "text": f"Prompt: {prompt}",
-    #                 "emoji": True
-    #             },
-    #             "image_url": image_url,
-    #             "alt_text": "Generated image"
-    #
-    #         }
-    #     ]
-    #
-    # )
+    client.chat_update(
+        channel=command["channel_id"],
+        ts=initial_message["ts"],
+        blocks= [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Image generated! :white_check_mark:. Prompt: {prompt}"
+            }
+            }
+        ]
+    )
 
 
+# Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, config("SLACK_APP_TOKEN")).start()
+    SocketModeHandler(app, os.environ['SLACK_APP_TOKEN']).start()
